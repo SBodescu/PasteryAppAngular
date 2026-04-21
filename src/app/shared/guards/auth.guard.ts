63a39c961f68154fcf776cd830ae7f5262d926e3
SBodescu <stefan.bodescu@cognizant.com>
2026-04-21 10:49:41 +0300

    feat:admin guard added on admin-dashboard route

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,12 +1,38 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { map, take } from 'rxjs/operators';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
+  const router = inject(Router);
   return authService.currentUser$.pipe(
     take(1),
-    map((user) => !!user),
+    map((user) => {
+      const isAuth = !!user;
+      if (isAuth) {
+        return true;
+      } else {
+        router.navigate(['/not-found']);
+        return false;
+      }
+    }),
+  );
+};
+
+export const adminGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  return authService.currentUser$.pipe(
+    take(1),
+    map((user) => {
+      const isAdmin = user?.user_metadata?.['role'] === 'admin';
+      if (isAdmin) {
+        return true;
+      } else {
+        router.navigate(['/not-found']);
+        return false;
+      }
+    }),
   );
 };
