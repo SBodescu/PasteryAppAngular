6a0594eca4f2d6371139a5e1aa72985c2d6d6e60
SBodescu <stefan.bodescu@cognizant.com>
2026-04-22 15:02:48 +0300

    feat: products and orders pagination added

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -11,8 +11,15 @@ import { ProductsService } from './services/products.service';
 })
 export class ProductsComponent implements OnInit {
   productsList: Product[] = [];
+
+  displayedItems: Product[] = [];
+  itemsPerPage: number = 6;
+  page: number = 0;
+  totalPages: number = 0;
+
   isLoading: boolean = true;
   errorMessage: string | null = null;
+
   private productsService = inject(ProductsService);
   private destroyRef = inject(DestroyRef);
 
@@ -29,11 +36,37 @@ export class ProductsComponent implements OnInit {
     const prodSubscription = this.productsService.products$.subscribe({
       next: (data) => {
         this.productsList = data.filter((p) => p.isDeleted !== true);
+        this.totalPages =
+          Math.ceil(this.productsList.length / this.itemsPerPage) || 1;
+        this.updatePagination();
       },
     });
+
     this.destroyRef.onDestroy(() => {
       fetchSubscription.unsubscribe();
       prodSubscription.unsubscribe();
     });
+  }
+
+  updatePagination() {
+    const startIndex = this.page * this.itemsPerPage;
+    this.displayedItems = this.productsList.slice(
+      startIndex,
+      startIndex + this.itemsPerPage,
+    );
+  }
+
+  pageDown() {
+    if (this.page > 0) {
+      this.page -= 1;
+      this.updatePagination();
+    }
+  }
+
+  pageUp() {
+    if (this.page < this.totalPages) {
+      this.page += 1;
+      this.updatePagination();
+    }
   }
 }
