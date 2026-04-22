97950558fe224f737ce3ce8d14bce7a68f222648
SBodescu <stefan.bodescu@cognizant.com>
2026-04-22 16:23:40 +0300

    feat: products filtering added

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -2,15 +2,26 @@ import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { ProductComponent } from './components/product/product.component';
 import { Product } from './models/product.model';
 import { ProductsService } from './services/products.service';
+import { FiltersComponent } from './components/filters/filters.component';
+import { FiltersService } from './services/filters.service';
+import { Filter } from './models/filter.model';
 
 @Component({
   selector: 'app-products',
-  imports: [ProductComponent],
+  imports: [ProductComponent, FiltersComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
 })
 export class ProductsComponent implements OnInit {
   productsList: Product[] = [];
+
+  filteredItems: Product[] = [];
+  filters: Filter = {
+    search: '',
+    category: 'all',
+    price: '',
+    alphabetical: 'none',
+  };
 
   displayedItems: Product[] = [];
   itemsPerPage: number = 6;
@@ -20,6 +31,7 @@ export class ProductsComponent implements OnInit {
   isLoading: boolean = true;
   errorMessage: string | null = null;
 
+  private filtersService = inject(FiltersService);
   private productsService = inject(ProductsService);
   private destroyRef = inject(DestroyRef);
 
@@ -36,9 +48,7 @@ export class ProductsComponent implements OnInit {
     const prodSubscription = this.productsService.products$.subscribe({
       next: (data) => {
         this.productsList = data.filter((p) => p.isDeleted !== true);
-        this.totalPages =
-          Math.ceil(this.productsList.length / this.itemsPerPage) || 1;
-        this.updatePagination();
+        this.applyFiltersAndPagination();
       },
     });
 
@@ -48,9 +58,25 @@ export class ProductsComponent implements OnInit {
     });
   }
 
+  onFiltersUpdated(newFilters: Filter) {
+    this.filters = newFilters;
+    this.page = 0;
+    this.applyFiltersAndPagination();
+  }
+
+  applyFiltersAndPagination() {
+    this.filteredItems = this.filtersService.filterAndSortProducts(
+      this.productsList,
+      this.filters,
+    );
+    this.totalPages =
+      Math.ceil(this.filteredItems.length / this.itemsPerPage) || 1;
+    this.updatePagination();
+  }
+
   updatePagination() {
     const startIndex = this.page * this.itemsPerPage;
-    this.displayedItems = this.productsList.slice(
+    this.displayedItems = this.filteredItems.slice(
       startIndex,
       startIndex + this.itemsPerPage,
     );
